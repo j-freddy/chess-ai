0b7760eea966d61362f50df033a96a9f09b22761
Freddy Jiang <jtf.imperial@gmail.com>
2024-01-02 19:51:48 +0000

    First draft of MCTS

--- ai/ai_mcts.py
+++ ai/ai_mcts.py
@@ -9,6 +9,18 @@ from player import Player
 State: TypeAlias = str
 Action: TypeAlias = chess.Move
 
+def result(board: chess.Board) -> float:
+    r = board.result()
+
+    if r == "1-0":
+        return 1.0
+    elif r == "0-1":
+        return -1.0
+    elif r == "1/2-1/2":
+        return 0.0
+    
+    return None
+
 def ucb_score(parent: Node, child: Node) -> float:
     prior_score = child.prior * math.sqrt(parent.visit_count) /\
         (child.visit_count + 1)
@@ -69,15 +81,12 @@ class Node:
                 )
 
 class AIMCTS(Player):
-    def backprop(self, search_path, value, current_player: chess.Color):
-        return NotImplemented
-
     def run(
         self,
         state: State,
         current_player: chess.Color,
         num_simulations=10,
-    )-> Node:
+    ) -> Node:
         """
         Perform Monte Carlo tree search: run @self.num_simulations simulations
         starting from board state @state.
@@ -99,14 +108,43 @@ class AIMCTS(Player):
             while node.is_expanded():
                 action, node = node.select_child()
                 search_path.append(node)
+
+            # TODO Test current player is indeed correct
+            current_player_at_leaf: chess.Color =\
+                current_player ^ len(search_path) % 2
             
             parent = search_path[-2]
             state = parent.state
 
-            # TODO
-            assert False
+            # We are now at a leaf node
+            # Make a move
+            board_at_leaf_node = chess.Board(state)
+            board_at_leaf_node.push(action)
+            next_state = board_at_leaf_node.fen()
+
+            # Get value of next state from perspective of White
+            value = result(board_at_leaf_node)
+
+            if value is None:
+                # Game has not ended
+                # Stage: EXPAND
+                actions = list(board_at_leaf_node.legal_moves)
+                action_probs = np.ones(len(actions)) / len(actions)
+                node.expand(next_state, current_player_at_leaf, action_probs)
+
+            # Get value from perspective of other player
+            if current_player_at_leaf == chess.BLACK:
+                value *= -1
+
+            self.backprop(search_path, value, parent.current_player ^ 1)
 
         return root
+
+    def backprop(self, search_path, value, current_player: chess.Color):
+        for node in reversed(search_path):
+            node.value_sum += value if node.current_player == current_player\
+                else -value
+            node.visit_count += 1
 
     def choose_move(self, position: str) -> str:
         root = self.run(position, self.color)
@@ -114,5 +152,8 @@ class AIMCTS(Player):
         return NotImplemented
 
 if __name__ == "__main__":
-    ai = AIMCTS(chess.WHITE)
-    ai.choose_move(chess.STARTING_FEN)
+    # ai = AIMCTS(chess.WHITE)
+    # ai.choose_move(chess.STARTING_FEN)
+
+    SCHOLARS_MATE = "rnb1kbnr/pppp1ppp/8/4p3/6Pq/5P2/PPPPP2P/RNBQKBNR w KQkq - 1 3"
+    print(result(chess.Board(SCHOLARS_MATE)))
