9fb36361c3e01b9717b62bd1189a13e34b13c406
Freddy Jiang <jiang.tf.2020@gmail.com>
2026-08-31 22:46:57 +0100

    Fix tests

--- ai/model_naive.py
+++ ai/model_naive.py
@@ -3,43 +3,40 @@ import numpy as np
 
 from ai.model import Action, Model, State
 
+CHECKMATE_VALUE = 10000
+piece_to_value: dict[chess.PieceType, float] = {
+    chess.PAWN: 1.0,
+    chess.KNIGHT: 3.0,
+    chess.BISHOP: 3.0,
+    chess.ROOK: 5.0,
+    chess.QUEEN: 9.0,
+}
+PRIOR_OFFSET = 1.0
+
+
+def statically_score_move(move: chess.Move, fen: str) -> float:
+    board = chess.Board(fen)
+    board.push(move)
+
+    if board.is_checkmate():
+        return CHECKMATE_VALUE
+
+    # Check captures
+    board.pop()
+
+    if board.is_capture(move):
+        piece_type = board.piece_type_at(move.to_square)
+
+        if piece_type is None:
+            assert board.is_en_passant(move)
+            return piece_to_value[chess.PAWN]
+
+        return piece_to_value[piece_type]
+
+    return 0.0
+
 
 class ModelNaive(Model):
-    def __init__(self):
-        super().__init__()
-
-        self.checkmate_value = 10000
-        self.piece_to_value: dict[chess.PieceType, float] = {
-            chess.PAWN: 1.0,
-            chess.KNIGHT: 3.0,
-            chess.BISHOP: 3.0,
-            chess.ROOK: 5.0,
-            chess.QUEEN: 9.0,
-        }
-
-        self._prior_offset = 1.0
-
-    def _statically_score_move(self, move: chess.Move, fen: str) -> float:
-        board = chess.Board(fen)
-        board.push(move)
-
-        if board.is_checkmate():
-            return self.checkmate_value
-
-        # Check captures
-        board.pop()
-
-        if board.is_capture(move):
-            piece_type = board.piece_type_at(move.to_square)
-
-            if piece_type is None:
-                assert board.is_en_passant(move)
-                return self.piece_to_value[chess.PAWN]
-
-            return self.piece_to_value[piece_type]
-
-        return 0.0
-
     def predict(self, state: State) -> list[tuple[Action, float]]:
         """
         Use static score evaluation to compute prior probabilities for each move
@@ -52,10 +49,7 @@ class ModelNaive(Model):
         prior = np.empty(len(actions))
 
         for i in range(len(actions)):
-            prior[i] = (
-                self._statically_score_move(actions[i], state)
-                + self._prior_offset
-            )
+            prior[i] = statically_score_move(actions[i], state) + PRIOR_OFFSET
 
         normalised_prior = prior / np.sum(prior)
         return list(zip(actions, normalised_prior))
